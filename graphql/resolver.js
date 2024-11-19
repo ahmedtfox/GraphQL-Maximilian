1f9f8a9f0a3e32eea8ef07d289955b86adff23d2
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-11-19 20:10:19 +0200

    426. Adding Input Validation

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,10 +1,30 @@
 const User = require("../model/user");
+const { isEmail, isEmpty, isLength } = require("validator");
+
 const createUser = async (args, req) => {
   const userInput = args.userInput;
   const email = userInput.email;
   const password = userInput.password;
   const name = userInput.name;
+
+  let errors = [];
+  if (!isEmail(email)) {
+    errors.push("Email is invalid");
+  }
+
+  if (!isEmpty(password)) {
+    errors.push("please input a password");
+  }
+
+  if (!isLength(password, { min: 5 })) {
+    errors.push("Email is too short!");
+  }
   try {
+    if (errors.length > 0) {
+      const error = new Error("Invalid Input.");
+      console.log(errors);
+      throw error;
+    }
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       const error = new Error("User exists already!");
